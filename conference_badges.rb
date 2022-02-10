e1ff5b6d41555b70b2fdfaca37d49ffe36741573
Catherine Loesch <catherine.loesch@protonmail.com>
2022-02-10 00:16:21 +0000

    refactoring

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,27 +1,16 @@
-
 def badge_maker(name)
     "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(arr)
-    arr.map do |name|
-        "Hello, my name is #{name}."
-    end
+    arr.map {|name| badge_maker(name)}
 end
 
 def assign_rooms(arr)
-    arr.map.with_index do |name, index|
-        "Hello, #{name}! You'll be assigned to room #{index+1}!"
-    end
+    arr.map.with_index(1) {|name, index| "Hello, #{name}! You'll be assigned to room #{index}!"}
 end
 
 def printer(arr)
-    arr.each do |name|
-        puts badge_maker(name)
-    end
-    
-    assign_rooms(arr).each do |message|
-        puts message
-    end
+    batch_badge_creator(arr).each {|badge| puts badge}
+    assign_rooms(arr).each {|message| puts message }
 end
-
